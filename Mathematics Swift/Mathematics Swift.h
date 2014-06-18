7c5166764c941731ae40dbd7d7daabed496f84a9
mctavishsolutions <aaron@Aarons-MacBook-Air.local>
2014-06-18 16:19:03 -0500

    Initial Commit

---
+++ Mathematics Swift/Mathematics Swift.h
@@ -0,0 +1,19 @@
+//
+//  Mathematics Swift.h
+//  Mathematics Swift
+//
+//  Created by Aaron McTavish on 6/18/14.
+//  Copyright (c) 2014 Aaron McTavish. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for Mathematics Swift.
+FOUNDATION_EXPORT double Mathematics_SwiftVersionNumber;
+
+//! Project version string for Mathematics Swift.
+FOUNDATION_EXPORT const unsigned char Mathematics_SwiftVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <Mathematics_Swift/PublicHeader.h>
+
+
